047c3e892e7c75b6dea2d3c2986cdc0e058d43cc
Ewan Mount <ejmount@users.noreply.github.com>
2018-09-04 20:18:29 +0100

    Refactored and made explicit benchmarking

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,47 @@ pub enum Instruction {
     Transfer(isize, isize),
 }
 
-static ITERS : usize = 1;
+fn benchmark(src: String) {
+    match parser::compile(src.chars(), false) {
+        Ok(prog) => {
+            
+        	let insts = prog.len();
+            let mut unopt_runner = interpreter::Program::new(prog);
+            let start = time::Instant::now();
+            let unopt_steps = unopt_runner.run(&mut std::io::stdin(), &mut std::io::stdout());
+            let end = time::Instant::now();
+
+            let opt_prog = parser::compile(src.chars(), true).unwrap();
+            let opt_insts = opt_prog.len();
+            let mut opt_runner = interpreter::Program::new(opt_prog);
+            let opt_start = time::Instant::now();
+            let opt_steps = opt_runner.run(&mut std::io::stdin(), &mut std::io::stdout());
+            let opt_end = time::Instant::now();
+
+            println!("{:?} unoptimized instructions, {:?} optimized", insts, opt_insts);
+
+            let unopt_time = end.duration_since(start);
+            let opt_time = opt_end.duration_since(opt_start);
+            println!(
+                "Ran {:?} unoptimized steps in {:?}, {:?} MIPS",
+                unopt_steps,
+                unopt_time,
+                (unopt_steps as f64 / (unopt_time.as_nanos() as f64) * 1e3)
+            );
+            println!(
+                "Ran {:?} optimized steps in {:?}, {:?} equivalent MIPS ({:?} real)",
+                opt_steps,
+                opt_time,
+                (unopt_steps as f64 / (opt_time.as_nanos() as f64) * 1e3),
+                (opt_steps as f64 / (opt_time.as_nanos() as f64) * 1e3)
+            );
+            println!("{:?}x speed improvement", unopt_time.as_nanos() as f64/(opt_time.as_nanos() as f64));
+        }
+        Err(i) => {
+            println!("Parse error at index {:?}", i);
+        }
+    }
+}
 
 fn main() {
     let fname = std::env::args().skip(1).next();
@@ -30,33 +70,7 @@ fn main() {
                     let mut src: String = String::new();
                     f.read_to_string(&mut src)
                         .expect("Failed to read file somehow");
-                    match parser::compile(src.chars()) {
-                        Ok(prog) => {
-                            
-                            println!("Got {:?} instructions", prog.len());
-                            let mut data :Vec<_> = (0..ITERS).map(|_| prog.clone()).collect();
-							let start = time::Instant::now();
-							let mut c = 0;
-							for i in 0..ITERS {
-								let a = std::mem::replace(&mut data[i], Vec::new());
-								let mut runner = interpreter::Program::new(a);
-                            	c += runner.run(&mut std::io::stdin(), &mut std::io::stdout());
-                            	//runner.print();
-                        	}
-                            //let c = 0;
-                            let end = time::Instant::now();
-                            let dur = end.duration_since(start);
-                            println!(
-                                "Ran {:?} steps in {:?}, {:?} MIPS",
-                                c,
-                                dur,
-                                (c as f64 / (dur.as_nanos() as f64) * 1e3)
-                            );
-                        }
-                        Err(i) => {
-                            println!("Parse error at index {:?}", i);
-                        }
-                    }
+                    benchmark(src);
                 }
                 Err(e) => {
                     println!("Could not open {:?}: {:?}", s, e);
